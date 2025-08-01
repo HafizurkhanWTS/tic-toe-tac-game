0741507415c3b5bb2c74a3651e80feb5ebbd7ce6
HafizurkhanWTS <r74129835@gmail.com>
2025-08-01 10:01:45 -0700

    gussing game

---
+++ guessing_game.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<time.h>
+
+
+int main(){
+    int random, guess;
+    int No_of_guess=0;
+    srand(time(NULL));
+    printf("welcome.. to The  World of gassing Number\n");
+    random= rand()%100 +1; // Nunber Btewwn (1-100)
+
+    do{
+        printf("\nplease enter your guess number between(1 to 100) : ");
+        scanf("%d",&guess);
+        No_of_guess++;
+
+        if(guess <random){
+            printf("Guess larger Number. \n");
+        } else if(guess> random){
+        printf("Guess smaller Number. \n");
+        } else{
+            printf("Congratulation...!! you have seccesfully guessed the Number in %d attempts\n",No_of_guess);
+        }
+    }while (guess != random);
+    {
+        printf("thanks for plying ...!!!");
+       
+    }
+   return 0; 
+}
